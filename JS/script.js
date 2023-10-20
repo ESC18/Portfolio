9c820b5b61753edfac19245d0a2b0028f7d99472
ESC18 <ponderosaprintingus@gmail.com>
2023-10-20 12:22:37 -0700

    (WIP) Adding Parallax functionality

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,6 @@
 // button1.addEventListener("click", (event) => {
 //     button1.style.color = "red";
 // });
+
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene);
