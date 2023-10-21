22219ad67aeb2f10f3c631d92bf7aa0d3d8b4735
ESC18 <ponderosaprintingus@gmail.com>
2023-10-20 20:38:14 -0700

    Add github link to cards

--- JS/projectData.js
+++ JS/projectData.js
@@ -1,12 +1,19 @@
 const data = {
     0: {
+        title: "Hello world",
+        description: "This is a fake description",
+        date: "04/05/2023",
+        github: "ksjdhfkljsdhf",
+        imgpath: "../ProjectImages/test.png",
+    },
+    1: {
         title: "",
         description: "",
         date: "",
         github: "",
         imgpath: "",
     },
-    1: {
+    2: {
         title: "",
         description: "",
         date: "",
