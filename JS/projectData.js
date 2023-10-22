9f488b21400a3ec74a874b988baad517f6a0e196
ESC18 <ponderosaprintingus@gmail.com>
2023-10-21 17:17:20 -0700

    (WIP) Fix Images

--- JS/projectData.js
+++ JS/projectData.js
@@ -15,7 +15,7 @@ const data = [
         description: "This is the new and revised version of my portfolio. It utilizes HTML, CSS, JS, and the Parallax API. This site will be consistently updated to display and enable access of and to web development tools and projects that I release.",
         date: "10/21/2023",
         github: "https://github.com/ESC18/Portfolio",
-        imgpath: "..Images/portfolio.png",
+        imgpath: "Images/portfolio.png",
         imgalt: "Image of this site - Portfolio website",
         update: false,
         updateDate: ""
@@ -25,7 +25,7 @@ const data = [
         description: "This nifty little site takes whatever you enter in and encodes it into a different language!! That is... a made up and unpronouncable language. The bottom text entry also shows the text retranslated so that way you can also decode incoming messages! The encoder automatically copies the translated message to your clipboard.",
         date: "6/8/2023",
         github: "https://github.com/ESC18/SecretLanguageEncoder",
-        imgpath: "..Images/encoder.png",
+        imgpath: "Images/encoder.png",
         imgalt: "This is an image of the secret language encoder",
         update: false,
         updateDate: ""
@@ -35,7 +35,7 @@ const data = [
         description: "This is my original Portfolio Site. It's an oldie and not quite as nice as the current one you're on but it's special!",
         date: "5/29/2023",
         github: "https://github.com/ESC18/Portfolio-Landing-Page",
-        imgpath: "..Images/oldPortfolio.png",
+        imgpath: "Images/oldPortfolio.png",
         imgalt: "this is an image of my original portfolio site",
         update: false,
         updateDate: ""
